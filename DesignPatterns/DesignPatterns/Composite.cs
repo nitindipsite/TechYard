1d8b964f15ea90508dcaf05ca8b66c27dfec21d1
Nitin Arora <nitindipsite@live.com>
2015-03-11 16:11:52 +1300

    Adding Composite pattern

---
+++ DesignPatterns/DesignPatterns/Composite.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns
+{
+    // Composite pattern is a specialization of Decorator pattern.
+    public interface IComponent
+    {
+        void SayMyName();
+    }
+
+    public class MenuItemComponent : IComponent
+    {
+        private readonly string _menuName;
+
+        public MenuItemComponent(string menuName)
+        {
+            _menuName = menuName;
+        }
+
+        public void SayMyName()
+        {
+            Console.WriteLine(string.Format("My name is {0}", _menuName));
+        }
+    }
+
+    public class MenuCompositeComponent : IComponent
+    {
+        private List<MenuItemComponent> children;
+
+        public MenuCompositeComponent()
+        {
+            children = new List<MenuItemComponent>();
+        }
+
+        public void AddComponent(MenuItemComponent childMenuItem)
+        {
+            children.Add(childMenuItem);
+        }
+
+        public void RemoveComponent(MenuItemComponent childMenuItem)
+        {
+            children.Remove(childMenuItem);
+        }
+
+        public void SayMyName()
+        {
+            foreach (MenuItemComponent childMenuItem in children)
+            {
+                childMenuItem.SayMyName();
+            }
+        }
+
+        public static void TestCompositePattern()
+        {
+            var menuComposite = new MenuCompositeComponent();
+            menuComposite.AddComponent(new MenuItemComponent("Save"));
+            menuComposite.AddComponent(new MenuItemComponent("Save As")); 
+            menuComposite.AddComponent(new MenuItemComponent("Delete"));
+
+            IComponent component = menuComposite;
+
+            component.SayMyName();
+        }
+    }
+
+    
+
+}
